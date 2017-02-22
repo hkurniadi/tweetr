8b883e490680c53c2fe29b646a0c3f5e07a84648
Hans Kurniadi <hans.d.k@live.com>
2017-02-22 19:44:22 +0000

    add avatar image on each posted tweet and three buttons that will appear when hovered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,56 +6,100 @@
 
 $(document).ready(function() {
    // Test / driver code (temporary). Eventually will get this from the server.
-   var tweetData = {
-     "user": {
-       "name": "Newton",
-       "avatars": {
-         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-       },
-       "handle": "@SirIsaac"
-     },
-     "content": {
-       "text": "If I have seen further it is by standing on the shoulders of giants"
-     },
-     "created_at": 1461116232227
-   }
+   var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
   function createTweetElement(obj) {
+    //Create <article> element:
     let $tweet = $('<article>').addClass('tweet');
 
-    //Children of <article>:
-    //Header and its children
+    //Create <header> element and its child elements
     let $header = $('<header>').addClass('tweet-header');
+    let $header_avatar = $('<img>').attr('src', obj['user']['avatars']['small'])
     let $header_h1 = $('<h1>').text(obj['user']['name']);
     let $header_p = $('<p>').text(obj['user']['handle']);
-    //$header_avatar
-    $($header).append($header_h1, $header_p);
+    $($header).append($header_avatar, $header_h1, $header_p);
     $($tweet).append($header);
 
-    //Section and its children
+    //Create <section> element and its child elements
     let $section = $('<section>').addClass('tweet-content');
     let $section_p = $('<p>').text(obj['content']['text']);
     $($section).append($section_p);
     $($tweet).append($section);
 
-    //Footer and its children
+    //Create <footer> element and its chil elements
     let $footer = $('<footer>').addClass('tweet-footer');
-    let createdAt = new Date(obj['created_at']);
-    let $footer_p = $('<p>').text(createdAt.getDay() + " days ago");
+    let createdAt = obj['created_at'];
+    let currentDate = Date.now();
+    let timeSinceLastCreated = Math.round((currentDate - createdAt)/86400000);
+    let $footer_p = $('<p>').text(timeSinceLastCreated + " days ago");
+    let $footer_span = $('<span>').addClass('hidden-buttons');
     let $footer_i1 = $('<i>').addClass('fa fa-flag').attr('aria-hidden', 'true');
     let $footer_i2 = $('<i>').addClass('fa fa-retweet').attr('aria-hidden', 'true');
     let $footer_i3 = $('<i>').addClass('fa fa-heart').attr('aria-hidden', 'true');
-    $($footer).append($footer_p, $footer_i1, $footer_i1, $footer_i2, $footer_i3);
+    $($footer_span).append($footer_i1, $footer_i2, $footer_i3);
+    $($footer).append($footer_p, $footer_span);
     $($tweet).append($footer);
 
     return $tweet;
   }
 
-  var $tweet = createTweetElement(tweetData);
+  function renderTweets(tweets) {
+    tweets.forEach((tweet) => {
+      let $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    });
+  }
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // var $tweet = createTweetElement(tweetData);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  renderTweets(data);
 });
